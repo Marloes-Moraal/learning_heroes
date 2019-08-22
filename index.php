fa90725ab51af256db0547f8debc0139e2715920
Marloes Moraal <moraal001@persgroep.net>
2019-08-22 23:21:18 +0200

    Added Ingredients

--- index.php
+++ index.php
@@ -5,37 +5,40 @@
     <title>Learning Heroes</title>
     <link rel="stylesheet" href="styles.css" type="text/css">
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" />
+    <script src="tailwind.config.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<nav>
-    <ul class="border-gray-400 background-grey w-screen py-20 m-0 sm:m-2 float-none sm:w-3/12 md:float-left ">
+<div class="border-gray-400 background-grey w-full py-20 m-0 sm:m-2 sm:w1/12 md:w-6/12 lg:w-3/12 float-left">
+    <ul>
         <li class="background-blue inline-block cursor-pointer list-none text-white text-sm text-center my-2 mx-10 py-2 px-24 w-10/12 rounded-sm">Home</li>
         <li class="background-blue inline-block cursor-pointer list-none text-white text-sm text-center my-2 mx-10 py-2 px-24 w-10/12 rounded-sm">About</li>
         <li class="background-blue inline-block cursor-pointer list-none text-white text-sm text-center my-2 mx-10 py-2 px-24 w-10/12 rounded-sm">Blog</li>
         <li class="background-blue inline-block cursor-pointer list-none text-white text-sm text-center my-2 mx-10 py-2 px-24 w-10/12 rounded-sm">Contact</li>
     </ul>
-</nav>
+</div>
 
-
-<table class="border-collapse float-left border-gray-400 py-4 px-4 m-10">
+<table class="border-collapse float-left border-gray-400 w-8/12 py-4 px-4 my-10 sm:m-10">
     <tr class="bg-teal-500 border-grey border">
-        <th class="py-2 px-12 text-center">
+        <th class="py-2 px-12 w-1/12 text-center">
             Cookies
         </th>
-        <th class="py-2 px-12 text-center">
+        <th class="py-2 px-12 w-1/12 text-center">
             Weight
         </th>
-        <th class="py-2 px-12 text-center">
-            Calories
+        <th class="py-2 px-12 w-1/12 text-center">
+            Calories (100g)
+        </th>
+        <th class="py-2 px-12 w-6/12 text-center">
+            Ingredients
         </th>
     </tr>
         <?php
 
             $servername = "localhost";
-            $username = "moraal";
-            $password = "password";
-            $dbname = "Cookies";
+            $username = "root";
+            $password = "root";
+            $dbname = "learning_heroes";
 
             // Create connection
                  $conn = new mysqli($servername, $username, $password, $dbname);
@@ -44,20 +47,46 @@
                     die("Connection failed: " . $conn->connect_error);
                   }
 
-                $sql = "SELECT Name, Weight, Calories FROM Cookies";
+                $sql = "SELECT * FROM cookies";
                 $result = $conn->query($sql);
 
                   if ($result->num_rows > 0) {
             // output data of each row
                 while($row = $result->fetch_assoc()) {
-                    echo "<tr class='border border-gray'><td class='py-2 text-center'>" . $row["Name"] . "</td><td class='py-2 text-center'>" . $row["Weight"] . " </td><td class='py-2 text-center'>" . $row["Calories"] . "</td></tr>";
+                $gramCookie = $row['calories'] / $row['weight'];
+                $caloriesPerhundredGram = $gramCookie * 100;
+                $cookieId = $row['id'];
+                $sql = "select * from cookie_ingredients left join ingredients on cookie_ingredients.ingredient_id = ingredients.id where cookie_ingredients.cookie_id = $cookieId order by name ASC";
+                $resultIngredienst = $conn->query($sql)->fetch_all(MYSQLI_ASSOC);
+                $ingredients = [];
+                    foreach($resultIngredienst as $ingredient) {
+                        $ingredients[] = $ingredient['name'];
+                    }
+                ?>
+                    <tr class='border border-gray'>
+                        <td class='py-2 text-center'>
+                            <?= $row["name"]; ?>
+                        </td>
+                        <td class='py-2 text-center'>
+                            <?= $row["weight"]; ?>
+                        </td>
+                        <td class='py-2 text-center'>
+                            <?= number_format($caloriesPerhundredGram, 0); ?>
+                        </td>
+                        <td class="">
+                            <?php
+                            echo implode(', ', $ingredients);
+                            ?>
+                        </td>
+                    </tr>
+                <?php
                 }
                 echo "</table>";
                 } else {
                     echo "0 results";
                 }
 
-            $conn->close();
+//            $conn->close();
 
         ?>
 </body>
