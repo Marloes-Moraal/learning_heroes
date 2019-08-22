170112b4ab9d31cc82cb8e7bab3564bcb989bdce
Marloes Moraal <moraal001@persgroep.net>
2019-08-22 17:39:32 +0200

    Changed some styling in tailwind CSS

--- index.php
+++ index.php
@@ -4,28 +4,29 @@
     <meta charset="UTF-8">
     <title>Learning Heroes</title>
     <link rel="stylesheet" href="styles.css" type="text/css">
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 <nav>
-    <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Blog</li>
-        <li>Contact</li>
+    <ul class="border-gray-400 background-grey w-screen py-20 m-0 sm:m-2 float-none sm:w-3/12 md:float-left ">
+        <li class="background-blue inline-block cursor-pointer list-none text-white text-sm text-center my-2 mx-10 py-2 px-24 w-10/12 rounded-sm">Home</li>
+        <li class="background-blue inline-block cursor-pointer list-none text-white text-sm text-center my-2 mx-10 py-2 px-24 w-10/12 rounded-sm">About</li>
+        <li class="background-blue inline-block cursor-pointer list-none text-white text-sm text-center my-2 mx-10 py-2 px-24 w-10/12 rounded-sm">Blog</li>
+        <li class="background-blue inline-block cursor-pointer list-none text-white text-sm text-center my-2 mx-10 py-2 px-24 w-10/12 rounded-sm">Contact</li>
     </ul>
 </nav>
 
 
-<table class="border-collapse float-left border-gray-400 py-4 px-4 ">
-    <tr>
+<table class="border-collapse float-left border-gray-400 py-4 px-4 m-10">
+    <tr class="bg-teal-500 border-grey border">
         <th class="py-2 px-12 text-center">
             Cookies
         </th>
-        <th>
+        <th class="py-2 px-12 text-center">
             Weight
         </th>
-        <th>
+        <th class="py-2 px-12 text-center">
             Calories
         </th>
     </tr>
@@ -49,7 +50,7 @@
                   if ($result->num_rows > 0) {
             // output data of each row
                 while($row = $result->fetch_assoc()) {
-                    echo "<tr><td>" . $row["Name"] . "</td><td>" . $row["Weight"] . " </td><td>" . $row["Calories"] . "</td></tr>";
+                    echo "<tr class='border border-gray'><td class='py-2 text-center'>" . $row["Name"] . "</td><td class='py-2 text-center'>" . $row["Weight"] . " </td><td class='py-2 text-center'>" . $row["Calories"] . "</td></tr>";
                 }
                 echo "</table>";
                 } else {
